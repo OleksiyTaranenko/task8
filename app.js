a43fb492a9b0c184024169d8abeb495068fc06f5
OleksiyTaranenko <oleksiytaranenko01@gmail.com>
2022-06-27 23:34:02 +0300

    task8 Epam lab

---
+++ app.js
@@ -0,0 +1,72 @@
+/* eslint-disable no-magic-numbers */
+// #1
+function extractCurrencyValue(param) {
+    let num = param.split(' ').shift();
+    if (num.length >= 16) {
+        return BigInt(num);
+    } else {
+        return num;
+    }
+}
+console.log(extractCurrencyValue('120 USD')); // 120
+console.log(extractCurrencyValue('1283948234720742 EUR')); // 1283948234720742n
+
+
+// #2
+
+let object = {
+    name: 'Ann',
+    age: 16,
+    hobbies: undefined,
+    degree: null,
+    isChild: false,
+    isTeen: true,
+    isAdult: false
+}
+
+function clearObject(obj) {
+    for (let key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            if (obj[key] === false || obj[key] === null || obj[key] === undefined) {
+                delete obj[key];
+            }        
+        }
+    }
+    return obj;    
+}
+
+console.log(clearObject(object)); // { name: 'Ann', age: 16, isTeen: true }
+
+
+// #3
+
+function getUnique(param) {
+    return Symbol(param);
+} 
+
+console.log(getUnique('Test')) // Symbol('Test')
+
+
+// #4
+
+function countBetweenTwoDays(startDate, endDate) {
+    let dateDifference = new Date(new Date(endDate).getTime() - new Date(startDate).getTime()).getTime();
+    let daysDifference = Math.ceil(dateDifference / (1000 * 60 * 60 * 24));
+    let weekDifference = parseInt(daysDifference / 7);
+    let monthDifference = parseInt(daysDifference / 24);
+    // eslint-disable-next-line max-len
+    console.log(`The difference between dates is: ${daysDifference} day(-s), ${weekDifference} week(-s), ${monthDifference} month(-s)`);
+}
+
+console.log(countBetweenTwoDays('03/22/22', '05/25/22')); // The difference between dates is: 64 day(-s), 9 week(-s), 2 month(-s)
+
+
+// #5
+
+function filterArray(arr) {
+    let uniqueArray = [...new Set(arr)];
+    let filteredArray = arr.filter((item, index) => arr.indexOf(item) === index);
+    return [uniqueArray, filteredArray];
+}
+
+console.log(filterArray([1, 2, 2, 4, 5, 5, 5, 6, 6, 7, 7, 8, 8, 8, 9])); // [1, 2, 3, 4, 5, 6, 7, 8, 9]
